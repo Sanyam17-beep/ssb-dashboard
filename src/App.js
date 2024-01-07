d71b6e39a76188049b310153c1b26eb1a66f4ffb
Sanyam Sharma <84903641+Sanyam17-beep@users.noreply.github.com>
2024-01-07 12:03:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import GoogleSheetTable from "./GoogleSheettable";
 export default function App() {
+   useEffect(() => {
+    const reloadInterval = 10000; // 10 seconds in milliseconds
+
+    const reloadPage = () => {
+      window.location.reload(true); // Reloads the page from the server
+    };
+
+    const reloadTimer = setTimeout(reloadPage, reloadInterval);
+
+    return () => {
+      // Clear the timeout when the component is unmounted to avoid memory leaks
+      clearTimeout(reloadTimer);
+    };
+  }, []);
   return (
     <div className="App">
       <GoogleSheetTable/>
